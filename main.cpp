5828fbdca49f90a0d5f716f2eeab4f01a89a758a
Arigatory Chuwi <arigatory@gmail.com>
2020-11-23 19:47:51 +0300

    my correct solution

--- main.cpp
+++ main.cpp
@@ -4,11 +4,27 @@ using namespace std;
 
 char *resize(const char *str, unsigned size, unsigned new_size)
 {
-	/* ... */
+	char * res = new char[new_size];
+    int temp_size = size;
+    if (new_size<size)
+    {
+        temp_size = new_size;
+    }
+    
+    for (const char * p = str; p < str + temp_size; ++p)
+    {
+        res[p-str] = *p;
+    }  
+    delete [] str;
+    return res;
 }
 
 
 int main()
 {
-    cout << "test\n";
+    int * n = new int(5);
+    cout << *n << " " << n << endl;
+    delete n;
+    n = new int[1000];
+    delete n;
 }
