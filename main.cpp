6cae5b791d502b4699aa5321c5fe63e5c5691ed6
Иван Иван <remillius_dies@outlook.com>
2020-12-01 07:29:10 +0300

    add task to solve

--- main.cpp
+++ main.cpp
@@ -324,6 +324,39 @@ Foo get_foo(const char *msg) {
     return Foo(Foo2(msg));
 }
 
+struct Expression
+{
+    virtual double evaluate() const = 0;
+};
+
+struct Number : Expression
+{
+    Number(double value)
+        : value(value)
+    {}
+
+private:
+    double value;
+};
+
+struct BinaryOperation : Expression
+{
+    /*
+      Здесь op это один из 4 символов: '+', '-', '*' или '/', соответствующих операциям,
+      которые вам нужно реализовать.
+     */
+    BinaryOperation(Expression const * left, char op, Expression const * right)
+        : left(left), op(op), right(right)
+    { }
+
+private:
+    Expression const * left;
+    Expression const * right;
+    char op;
+};
+
+
+
 int main()
 {
     char msg[] = "hi";
