1867a8143f6f33ae813e6903734f71fdd1d9e4e4
Arigatory Chuwi <arigatory@gmail.com>
2020-12-02 12:35:16 +0300

    in constructor on-virtual method call example

--- main.cpp
+++ main.cpp
@@ -327,6 +327,7 @@ Foo get_foo(const char *msg) {
 struct Expression
 {
     virtual double evaluate() const = 0;
+    virtual ~Expression(){};
 };
 
 struct Number : Expression
@@ -334,7 +335,10 @@ struct Number : Expression
     Number(double value)
         : value(value)
     {}
-
+    double evaluate() const 
+    {
+        return this->value;
+    }
 private:
     double value;
 };
@@ -348,7 +352,23 @@ struct BinaryOperation : Expression
     BinaryOperation(Expression const * left, char op, Expression const * right)
         : left(left), op(op), right(right)
     { }
-
+    double evaluate() const 
+    {
+        if (op == '+') {
+            return this->left->evaluate() + this->right->evaluate();
+        } else if (op == '-') {
+            return this->left->evaluate() - this->right->evaluate();
+        } else if (op == '*') {
+            return this->left->evaluate() * this->right->evaluate();
+        } else if (op == '/') {
+            return this->left->evaluate() / this->right->evaluate();
+        } 
+        return 0;
+    }
+    virtual ~BinaryOperation(){
+        delete left;
+        delete right;
+    }
 private:
     Expression const * left;
     Expression const * right;
@@ -359,6 +379,15 @@ private:
 
 int main()
 {
-    char msg[] = "hi";
-    foo_says(get_foo(msg));
+    // сначала создаём объекты для подвыражения 4.5 * 5
+Expression * sube = new BinaryOperation(new Number(4.5), '*', new Number(5));
+// потом используем его в выражении для +
+Expression * expr = new BinaryOperation(new Number(3), '+', sube);
+
+// вычисляем и выводим результат: 25.5
+std::cout << expr->evaluate() << std::endl;
+
+// тут освобождаются *все* выделенные объекты
+// (например, sube будет правым операндом expr, поэтому его удалять не нужно)
+delete expr;
 }
---
+++ persons.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+struct Person {
+    Person(string const& name, int age)
+        : name_(name), age_(age)
+    {}
+    virtual ~Person() {}
+    virtual string name() const {return name_;}
+    virtual string ocupation() const = 0;
+
+
+private:
+    string name_;
+    int age_;
+};
+
+struct Teacher : Person {
+    Teacher(string const& nm, int age, string const& course)
+        :Person(nm, age), course_(course)
+        {
+            cout << name() << endl;
+        }
+
+        virtual string ocupation() const {return "teacher"; }
+        virtual string course() const {return course_;}
+
+private:
+    string course_;
+};
+
+struct Professor : Teacher {
+    Professor(string const& name, int age, string const& course,
+              string const& thesis)
+        : Teacher(name, age, course), thesis_(thesis)
+    {}
+
+    virtual string thesis() const { return thesis_; }
+private:
+    string thesis_;
+};
+
+int main()
+{
+    Professor p("Stroustrup", 63, "C++", "The C++ language");
+    return 0;
+}
