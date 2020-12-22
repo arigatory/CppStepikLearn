03e990d4070555447abdf43997dc7eea06df062d
Ivan Panchenko <arigatory@gmail.com>
2020-12-22 05:02:12 +0300

    before check template array

--- main.cpp
+++ main.cpp
@@ -680,6 +680,64 @@ private:
 };
 
 
+template <typename T>
+class Array
+{
+    T * data_;
+    size_t size_;
+public:
+    explicit Array(size_t size = 0, const T& value = T())
+    :size_(size)
+    ,data_(new T[size]){
+        for (size_t i = 0; i < size_; i++)
+        {
+            data_[i] = value;
+        }
+    }
+
+
+
+    Array(const Array & a)
+        :size_(a.size_), data_(new T[a.size_]){
+            for (size_t i = 0; i < size_; i++)
+            {
+                data_[i] = a.data_[i];
+            } 
+    }
+
+    ~Array(){
+        delete [] data_;
+    }
+
+    Array& operator=(Array const& a){
+        if (this != &a)
+        {
+            Array(a).swap(*this);
+            return *this;
+        }
+    }
+    
+    void swap(Array & a) {
+        swap(size_, a.size_);
+        swap(data_,a.data_);
+    }
+    
+    size_t size() const{
+        return size_;
+    }
+
+
+    T& operator[](size_t i){
+        return data_[i];
+    }
+
+    const T& operator[](size_t i) const {
+        return data_[i];
+    }
+   
+};
+
+
 int main()
 {
     SharedPtr p1;
